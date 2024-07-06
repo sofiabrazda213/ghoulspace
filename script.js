869901db1a96b654176c928e227d64dd55959c86
sofia <sofiajaimeb@gmail.com>
2024-07-05 20:59:59 -0700

    Tab bar

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Die, Fucker!');
-});
+function openTab(event, tabName) {
+    var i, tabContent, tabButton;
+    
+    // Hide all tab content
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    
+    // Remove the "active" class from all tab buttons
+    tabButton = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButton.length; i++) {
+        tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+    
+    // Show the current tab content and add "active" class to the clicked tab button
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
