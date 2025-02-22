812d4b07f47599c6d47ebb4471ff64e40b700819
sofia <sofiajaimeb@gmail.com>
2025-02-21 16:12:58 -0800

    tab

--- script.js
+++ script.js
@@ -11,26 +11,6 @@ document.getElementById("toggleBtn").addEventListener("click", function () {
   }
 });
 
-function openTab(event, tabName) {
-    var i, tabContent, tabButton;
-    
-    // Hide all tab content
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
-    
-    // Remove the "active" class from all tab buttons
-    tabButton = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tabButton.length; i++) {
-        tabButton[i].className = tabButton[i].className.replace(" active", "");
-    }
-    
-    // Show the current tab content and add "active" class to the clicked tab button
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
-}
-
 // Get the modal
 var modal = document.getElementById("imageModal");
 
